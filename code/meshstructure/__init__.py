827f4312c541f601d6c02c61df1479b835f9d9b3
Lawrence Mitchell <lawrence@wence.uk>
2019-09-17 08:44:54 +0100

    Pacify linter

--- code/meshstructure/__init__.py
+++ code/meshstructure/__init__.py
@@ -1,7 +1,9 @@
-from .datalayout import *
-from .extrusion import *
-from .geometry import *
-from .hypercube import *
-from .symbolics import *
-from .topology import *
-from .unstructured import *
+# Don't care about formatting issues in this file
+# flake8: noqa
+from meshstructure.datalayout import *
+from meshstructure.extrusion import *
+from meshstructure.geometry import *
+from meshstructure.hypercube import *
+from meshstructure.symbolics import *
+from meshstructure.topology import *
+from meshstructure.unstructured import *
